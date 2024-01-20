76a780b82c58909d6ff4281848fe912fce51969b
MJDevelops <joey.meierkord@gmail.com>
2024-01-20 23:11:07 +0100

    Various changes

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,6 @@ pub mod tui;
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-
     #[test]
     fn some_test() {
         assert_eq!(2 + 2, 4);
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
-use rspotify::{clients::OAuthClient, AuthCodeSpotify};
-use rustify::{app, auth::*};
+use rspotify::{clients::OAuthClient, AuthCodeSpotify, Token};
+use rustify::auth::*;
 use std::{error::Error, io::Write};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     let spotify = init_spotify();
-    let _ = get_token_auto(&spotify).await;
+    let token = Token::from_cache(&spotify.config.cache_path);
 
-    println!("{:?}", *spotify.token.lock().await.unwrap());
+    match token {
+        Ok(_) => {
+            // Check if token is expired and refresh it if needed
+            let _ = refresh_auth_code(&spotify).await;
+        }
+        Err(_) => {
+            let _ = get_token_auto(&spotify).await;
+        }
+    }
+
+    let token = spotify.read_token_cache(false).await.unwrap();
+    *spotify.token.lock().await.unwrap() = token;
+
+    println!("{:?}", spotify.pause_playback(None).await);
 
     Ok(())
 }
@@ -23,10 +36,10 @@ async fn get_token_auto(spotify: &AuthCodeSpotify) -> Result<(), &'static str> {
     let _ = std::io::stdout().flush();
 
     if let Ok(_) = std::io::stdin().read_line(&mut input) {
-        let code = spotify.parse_response_code(&url).unwrap();
+        let code = spotify.parse_response_code(&input).unwrap();
         match spotify.request_token(&code).await {
             Ok(_) => return Ok(()),
-            Err(e) => return Err("Error requesting token"),
+            Err(_) => return Err("Error requesting token"),
         }
     } else {
         return Err("Couldn't parse URL");
